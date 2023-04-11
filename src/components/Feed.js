54aa029d84f8adce00d2a1e914b81f1a3ae4d7ad
MaimoonHossain <ashikasitosh740@gmail.com>
2023-04-12 00:49:34 +0600

    fourth commit

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Box } from "@mui/material";
+import Post from "./Post";
 
 const Feed = () => {
   return (
-    <Box
-      bgcolor="pink"
-      flex={4}
-      p={2}
-      sx={{
-        display: { sm: "block" },
-      }}
-    >
-      Feed
+    <Box flex={4} p={2}>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </Box>
   );
 };
